180b531b8b799829ca5488c64d123c37bda25630
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-12-19 15:19:18 +0700

    npm pack

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,20 +1,20 @@
 import { createAction } from '@reduxjs/toolkit';
-import shortid from 'shortid';
+// import shortid from 'shortid';
 
-const addContact = createAction('contactList/add', (name, number) => ({
-    payload: {
-        id: shortid.generate(),
-        name,
-        number,
-    },
-}));
+// const addContact = createAction('contactList/add', (name, number) => ({
+//     payload: {
+//         id: shortid.generate(),
+//         name,
+//         number,
+//     },
+// }));
 
-const deleteContact = createAction('contactList/delete', contactId => ({
-    payload: contactId,
-}));
+// const deleteContact = createAction('contactList/delete', contactId => ({
+//     payload: contactId,
+// }));
 
 const filterContacts = createAction('contactList/filter', contact => ({
     payload: contact,
 }));
 
-export { addContact, deleteContact, filterContacts };
+export { filterContacts };
